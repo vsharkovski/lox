92ae80345c900ae4b90888a151a5a8e0389838f6
Vladimir Sharkovski <vsarkovskii@gmail.com>
2022-12-27 17:40:08 +0100

    simplify code

--- klox/src/main/kotlin/com/vsharkovski/klox/AstPrinterMultiline.kt
+++ klox/src/main/kotlin/com/vsharkovski/klox/AstPrinterMultiline.kt
@@ -2,81 +2,68 @@ package com.vsharkovski.klox
 
 private const val INDENTATION = "  "
 
-class AstPrinterMultiline : Stmt.Visitor<String>, Expr.Visitor<String> {
-    fun print(stmt: Stmt) = stmt.accept(this)
 
-    fun print(expr: Expr) = expr.accept(this)
+class AstPrinterMultiline : Stmt.Visitor<String>, Expr.Visitor<String> {
+    fun print(stmt: Stmt): String = stmt.accept(this)
+
+    fun print(expr: Expr): String = expr.accept(this)
+
+    private fun printIndented(stmt: Stmt): String =
+        print(stmt).prependIndent(INDENTATION)
+
+    private fun printIndented(expr: Expr): String =
+        print(expr).prependIndent(INDENTATION)
 
     override fun visitBlockStmt(stmt: Stmt.Block): String =
         stmt.statements.fold("Block:") { result, curr ->
-            "${result}\n${curr.accept(this).prependIndent(INDENTATION)}"
+            "${result}\n${printIndented(curr)}"
         }
 
     override fun visitExpressionStmt(stmt: Stmt.Expression): String =
-        "Expression:\n${stmt.expression.accept(this).prependIndent(INDENTATION)}"
+        "Expression:\n${printIndented(stmt.expression)}"
 
     override fun visitIfStmt(stmt: Stmt.If): String = buildString {
-        append("If:\n(condition)\n")
-        append(stmt.condition.accept(this@AstPrinterMultiline).prependIndent(INDENTATION))
-        append("\n(then)\n")
-        append(stmt.thenBranch.accept(this@AstPrinterMultiline).prependIndent(INDENTATION))
-        if (stmt.elseBranch != null) {
-            append("\n(else)\n")
-            append(stmt.elseBranch.accept(this@AstPrinterMultiline).prependIndent(INDENTATION))
-        }
+        append("If:\n(condition)\n${printIndented(stmt.condition)}\n(then)\n${printIndented(stmt.thenBranch)}")
+        if (stmt.elseBranch != null)
+            append("\n(else)\n${printIndented(stmt.elseBranch)}")
     }
 
     override fun visitPrintStmt(stmt: Stmt.Print): String =
-        "Print:\n${stmt.expression.accept(this).prependIndent(INDENTATION)}"
+        "Print:\n${printIndented(stmt.expression)}"
 
     override fun visitVarStmt(stmt: Stmt.Var): String =
         when (stmt) {
             is Stmt.UninitializedVar ->
                 "UninitializedVar: ${stmt.name.lexeme}"
             is Stmt.InitializedVar ->
-                "InitializedVar: ${stmt.name.lexeme}, value:\n${
-                    (stmt.initializer?.accept(this) ?: "").prependIndent(INDENTATION)
-                }"
+                "InitializedVar: ${stmt.name.lexeme}, value:\n${printIndented(stmt.initializer)}"
         }
 
-    override fun visitAssignExpr(expr: Expr.Assign): String =
-        "Assign: ${expr.name.lexeme}, value:\n${
-            expr.value.accept(this).prependIndent(INDENTATION)
-        }"
+    override fun visitWhileStmt(stmt: Stmt.While): String =
+        "While:\ncondition:\n${printIndented(stmt.condition)}"
 
-    override fun visitBinaryExpr(expr: Expr.Binary): String = buildString {
-        append("Binary: ${expr.operator.lexeme}\n(left)\n")
-        append(expr.left.accept(this@AstPrinterMultiline).prependIndent(INDENTATION))
-        append("\n(right)\n")
-        append(expr.right.accept(this@AstPrinterMultiline).prependIndent(INDENTATION))
-    }
+    override fun visitAssignExpr(expr: Expr.Assign): String =
+        "Assign: ${expr.name.lexeme}, value:\n${printIndented(expr.value)}"
+
+    override fun visitBinaryExpr(expr: Expr.Binary): String =
+        "Binary: ${expr.operator.lexeme}\n(left)\n${printIndented(expr.left)}\n(right)\n${printIndented(expr.right)}"
 
     override fun visitGroupingExpr(expr: Expr.Grouping): String =
-        "Group:\n${expr.expression.accept(this).prependIndent(INDENTATION)}"
+        "Group:\n${printIndented(expr.expression)}"
 
     override fun visitLiteralExpr(expr: Expr.Literal): String =
         "Literal: ${if (expr.value == null) "nil" else expr.value.toString()}"
 
-    override fun visitLogicalExpr(expr: Expr.Logical): String = buildString {
-        append("Logical: ${expr.operator.lexeme}\n(left)\n")
-        append(expr.left.accept(this@AstPrinterMultiline).prependIndent(INDENTATION))
-        append("\n(right)\n")
-        append(expr.right.accept(this@AstPrinterMultiline).prependIndent(INDENTATION))
-    }
+    override fun visitLogicalExpr(expr: Expr.Logical): String =
+        "Logical: ${expr.operator.lexeme}\n(left)\n${printIndented(expr.left)}\n(right)\n${printIndented(expr.right)}"
 
-    override fun visitTernaryExpr(expr: Expr.Ternary): String = buildString {
-        append("Ternary:\n(condition)\n")
-        append(expr.left.accept(this@AstPrinterMultiline).prependIndent(INDENTATION))
-        append("\n(then)\n")
-        append(expr.middle.accept(this@AstPrinterMultiline).prependIndent(INDENTATION))
-        append("\n(else)\n")
-        append(expr.right.accept(this@AstPrinterMultiline).prependIndent(INDENTATION))
-    }
+    override fun visitTernaryExpr(expr: Expr.Ternary): String =
+        "Ternary:\n(condition)\n${printIndented(expr.left)}\n(then)\n${printIndented(expr.middle)}\n(else)\n${
+            printIndented(expr.right)
+        }"
 
     override fun visitUnaryExpr(expr: Expr.Unary): String =
-        "Unary: ${expr.operator.lexeme}, right:\n${
-            expr.right.accept(this).prependIndent(INDENTATION)
-        }"
+        "Unary: ${expr.operator.lexeme}, right:\n${printIndented(expr.right)}"
 
     override fun visitVariableExpr(expr: Expr.Variable): String =
         "Var: ${expr.name.lexeme}"
