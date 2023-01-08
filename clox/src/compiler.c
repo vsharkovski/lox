45dca20f6e89052f0d2a0874e0c49f514635d84a
Vladimir Sharkovski <vsarkovskii@gmail.com>
2023-01-08 20:02:21 +0400

    add code generation utility functions

--- clox/src/compiler.c
+++ clox/src/compiler.c
@@ -14,6 +14,12 @@ typedef struct
 } Parser;
 
 Parser parser;
+Chunk* compilingChunk;
+
+static Chunk* currentChunk()
+{
+    return compilingChunk;
+}
 
 static void errorAt(Token* token, const char* message)
 {
@@ -73,9 +79,31 @@ static void consume(TokenType type, const char* message)
     errorAtCurrent(message);
 }
 
+static void emitByte(uint8_t byte)
+{
+    writeChunk(currentChunk(), byte, parser.previous.line);
+}
+
+static void emitBytes(uint8_t byte1, uint8_t byte2)
+{
+    emitByte(byte1);
+    emitByte(byte2);
+}
+
+static void emitReturn()
+{
+    emitByte(OP_RETURN);
+}
+
+static void endCompiler()
+{
+    emitReturn();
+}
+
 bool compile(const char* source, Chunk* chunk)
 {
     initScanner(source);
+    compilingChunk = chunk;
 
     parser.hadError = false;
     parser.panicMode = false;
@@ -83,6 +111,6 @@ bool compile(const char* source, Chunk* chunk)
     advance();
     expression();
     consume(TOKEN_EOF, "Expect end of expression.");
-    
+    endCompiler();
     return !parser.hadError;
 }
