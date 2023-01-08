366d4662ef89b5a49e53345de3fe312562c77411
Vladimir Sharkovski <vsarkovskii@gmail.com>
2023-01-08 16:24:37 +0400

    add vm, interpreting, stack, numeric operations

--- clox/src/main.c
+++ clox/src/main.c
@@ -1,26 +1,38 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, const char* argv[])
 {
+    initVM();
+
     Chunk chunk;
     initChunk(&chunk);
 
     int constant = addConstant(&chunk, 1.2);
-    writeChunk(&chunk, OP_CONSTANT, 5);
-    writeChunk(&chunk, constant, 5);
+    writeChunk(&chunk, OP_CONSTANT, 55);
+    writeChunk(&chunk, constant, 55);
 
-    writeChunk(&chunk, OP_RETURN, 5);
-    writeChunk(&chunk, OP_RETURN, 5);
-    writeChunk(&chunk, OP_RETURN, 5);
+    constant = addConstant(&chunk, 3.4);
+    writeChunk(&chunk, OP_CONSTANT, 55);
+    writeChunk(&chunk, constant, 55);
 
-    writeChunk(&chunk, OP_RETURN, 7);
-    
-    writeChunk(&chunk, OP_RETURN, 11);
-    writeChunk(&chunk, OP_RETURN, 11);
+    writeChunk(&chunk, OP_ADD, 55);
+
+    constant = addConstant(&chunk, 5.6);
+    writeChunk(&chunk, OP_CONSTANT, 55);
+    writeChunk(&chunk, constant, 55);
+
+    writeChunk(&chunk, OP_DIVIDE, 55);
+    writeChunk(&chunk, OP_NEGATE, 55);
+
+    writeChunk(&chunk, OP_RETURN, 55);
 
     disassembleChunk(&chunk, "test chunk");
+
+    interpret(&chunk);
+    freeVM();
     
     freeChunk(&chunk);
     return 0;
