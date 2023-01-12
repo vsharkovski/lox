dc0bf43dae7abd7f3c3ecce8457169930c23c6a6
Vladimir Sharkovski <vsarkovskii@gmail.com>
2023-01-12 21:36:25 +0400

    fix critical pointer bug

--- clox/src/table.c
+++ clox/src/table.c
@@ -123,7 +123,7 @@ static void adjustCapacity(Table* table, int capacity)
     table->count = 0;
     for (int i = 0; i < table->capacity; i++)
     {
-        Entry* entry = table + i;
+        Entry* entry = table->entries + i;
         if (entry->key == NULL) continue;
 
         Entry* dest = findEntry(entries, capacity, entry->key);
